e751a960ff483042d75a61d7e657b00e1bfee700
DanielRomero <dandres508@gmail.com>
2024-05-10 15:38:51 -0500

    portafolio

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkmode: "class",
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
